f6ae4019970e2b4b44e24141a5e073c4b5b72d9a
s3m1h <semih_acar01@hotmail.com>
2022-02-20 19:04:01 +0300

    9.Hafta Mola 1

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    // Entity implement eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -1,7 +1,5 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
