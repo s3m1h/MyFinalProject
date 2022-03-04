3c43e43b4db6ae71f606b3e2fbdfe025cc8bb4bd
s3m1h <semih_acar01@hotmail.com>
2022-03-04 10:05:15 +0300

    12.Ders Sonu

---
+++ Business/Concrete/CustomerManager.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    internal class CustomerManager : ICustomerService
+    {
+        ICustomerDal _customerDal;
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+        public IResult Add(Customer customer)
+        {
+            _customerDal.Add(customer);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(Customer customer)
+        {
+            _customerDal.Delete(customer);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<Customer>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
