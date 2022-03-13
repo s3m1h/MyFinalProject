c1324e4b50147e1f64e504fd5136194580139e7a
s3m1h <semih_acar01@hotmail.com>
2022-03-13 12:30:42 +0300

    14.Ders commit 1

--- Business/Concrete/CategoryManager.cs
+++ Business/Concrete/CategoryManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
@@ -18,14 +19,14 @@ namespace Business.Concrete
             _categoryDal = categoryDal;
         }
 
-        public List<Category> GetAll()
+        public IDataResult<List<Category>> GetAll()
         {
-            return _categoryDal.GetAll();
+            return new SuccessDataResult<List<Category>>(_categoryDal.GetAll());
         }
 
-        public Category GetById(int categoruId)
+        public IDataResult<Category> GetById(int categoruId)
         {
-            return _categoryDal.Get(c=>c.CategoryId == categoruId);
+            return new SuccessDataResult<Category>(_categoryDal.Get(c=>c.CategoryId == categoruId));
         }
     }
 }
