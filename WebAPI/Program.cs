f20f2ec18bc7b16127764aeb6d650ee148de4072
s3m1h <semih_acar01@hotmail.com>
2022-07-06 10:12:58 +0300

    .

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -17,7 +17,7 @@ using Core.DefendencyResolvers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
